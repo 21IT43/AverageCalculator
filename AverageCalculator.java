9cd19273d5c0bf711459183da4fcd7c66ae29ffb
21IT43 <94101362+21IT43@users.noreply.github.com>
2023-11-18 10:34:58 +0530

    Add files via upload

---
+++ AverageCalculator.java
@@ -0,0 +1,17 @@
+import java.util.scanner;
+public class Average Calculator() {
+   public static Void main (String [] args ) {
+     scanner scanner = new scanner(system.in);
+     system.out.println("Enter the value of n");
+     int n = scanner.nextInt();
+     system.out.println("Enter the value of elements ");
+     double sum=0;
+     for(int i=0; i<=n; i++) {
+       double number = scanner.nextDouble();
+       sum+=number;
+       }
+     double average = sum/n;  
+     system.out.println("The average of " + n + "is" + average);
+     scanner.close();
+    }
+   }
